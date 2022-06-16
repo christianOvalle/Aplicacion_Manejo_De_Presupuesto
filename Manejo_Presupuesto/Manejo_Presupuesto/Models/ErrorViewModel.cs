5eaf8ee3239f6ac78bba20c364045116afa1312f
christianOvalle <cristianovallegutierrez@gmail.com>
2022-06-16 19:05:57 -0400

    Asp Net Core

---
+++ Manejo_Presupuesto/Manejo_Presupuesto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Manejo_Presupuesto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Manejo_Presupuesto/Manejo_Presupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Manejo_Presupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo nombre es requerido")]
+        [StringLength(maximumLength: 40, MinimumLength = 3, ErrorMessage ="La longitud del campo nombre debe estar entre 50 y 3") ]
+        public string Nombre { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public int Orden { get; set; }
+
+    }
+}
